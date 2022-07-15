26d3ac0ab131423b2f01cf40fcdb4b6043790001
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-15 14:09:19 +0900

    11¨계

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -8,6 +8,16 @@ public class App {
         this.sc = sc;
     }
     void run(){
-
+        System.out.println("== 명언 SSG ==");
+        System.out.print("명령)");
+        String cmd = sc.nextLine();
+        Rq rq = new Rq(cmd);
+        outer:
+        while(true){
+            switch (rq.getPath()){
+                case "종료":
+                    break outer;
+            }
+        }
     }
 }
